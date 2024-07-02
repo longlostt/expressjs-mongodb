2df130b9faeb01bd355a315240a1f1da4dff2a6e
longlost <bg2811777@gmail.com>
2024-07-02 19:59:23 -0400

    added error message

--- models/product.js
+++ models/product.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'name cannot be blank!']
     },
     price:{
         type: Number,
-        required: true,
+        required: [true, 'price cannot be blank!'],
         min: 0
     },
     category: {
